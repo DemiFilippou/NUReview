efb9dd164f367cfca20719d6a18cbd078ce8ba4a
Demi Filippou <filippou.d@husky.neu.edu>
2019-03-01 18:45:26 -0500

    show endpoint for companies

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,11 +1,31 @@
 class CompaniesController < ApplicationController
+
+  def show
+    @company = Company.find(params[:id]) 
+    if @company.nil?
+      return render json: {
+        message: "Can't find company with id #{company_params[:id]}"
+      }, status: 404
+    end
+
+    render json: @company, except: [:updated_at, :created_at], include: :reviews
+  end
+
   def search
-    @companies = Company.search(params.require(:query))
+    @companies = Company.search(search_params)
     render json: @companies, only: [:id, :name]
   end
 
   private
   def company_params
     params.require(:company).permit!
+  end
+
+  def search_params
+    params.require(:query)
+  end
+  
+  def find_company
+    @company = Company.find(company_params[:id]) 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :tags, only: [:index]
 
 
-  resources :companies do
+  resources :companies, only: [:show] do
     resources :reviews, only: [:create, :update, :index] 
   end
 

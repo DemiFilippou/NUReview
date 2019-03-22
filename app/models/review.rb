b0e26c25a051e3ede2fe5211bc9b0d4501a1e714
Demi Filippou <filippou.d@husky.neu.edu>
2019-03-22 16:11:34 -0400

    users automatically upvote their own post

--- app/models/review.rb
+++ app/models/review.rb
@@ -15,7 +15,15 @@ class Review < ApplicationRecord
   validates :wage, :numericality => { :greater_than_or_equal_to => 0 }, allow_blank: true
   validates :enjoyment, :learning, :recommend, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 10 }, allow_blank: true
 
+  # users automatically upvote their own post
+  after_create :upvote_post
+
   enumerize :semester, in: [:Spring, :Fall, :Summer]
+
+  def upvote_post
+    binding.pry
+    Vote.create(user_id: self.user_id, review_id: self.id, value: 1)
+  end
 
   # total upvotes/downvotes score
   def recalculate_score(incoming_score)
